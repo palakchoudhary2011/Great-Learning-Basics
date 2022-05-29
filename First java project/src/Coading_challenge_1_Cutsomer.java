e82a0f7e69a983a96831b4c6432825f2c8ea2bd6
palak <palak@LAPTOP-69NGPM3G>
2022-05-29 22:47:26 +0530

    Coding challenge 1 - Class ad object

---
+++ First java project/src/Coading_challenge_1_Cutsomer.java
@@ -0,0 +1,18 @@
+
+public class Coading_challenge_1_Cutsomer {
+	public String fullname ;
+	public int age;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Coading_challenge_1_Cutsomer main = new Coading_challenge_1_Cutsomer();
+		main.fullname = "Palak CHoudhary";
+		main.age = 26;
+		
+		System.out.println("My name is " + main.fullname + "and my age is "+ main.age);
+	}
+}
+
+
